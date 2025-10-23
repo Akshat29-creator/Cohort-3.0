fa4f3d1003e0a8adc14455af70994ce4c59ad711
Akshat Awasthi <138347135+Akshat29-creator@users.noreply.github.com>
2025-10-24 01:17:02 +0530

    async and await code

---
+++ Callback hell, Rejects and async await/Await.js
@@ -0,0 +1,13 @@
+function setTimeoutPromisified(ms){
+    return new Promise((resolve)=>setTimeout(resolve,ms));
+}
+async function solve(){
+    await setTimeoutPromisified(1000);
+    console.log("hi");
+    await setTimeoutPromisified(3000);
+    console.log("hello");
+    await setTimeoutPromisified(5000);
+    console.log("hello there");
+}
+solve();
+console.log("This message is shown before the async function completes");
